7d6758ba277e068764186d14ec0e1552bea233ad
Muhammad Hamza <mhumza13420@gmail.com>
2026-01-26 05:34:59 +0500

    Implement GET route for retrieving recent orders with authentication and authorization checks

---
+++ app/api/admin/analytics/recent-orders/route.ts
@@ -0,0 +1,54 @@
+import { getRecentOrdersService } from "@/app/services/admin-analytics";
+import { db } from "@/lib/db";
+import { users } from "@/lib/db/schema/schemas";
+import { RecentOrder } from "@/types/analytics";
+import { ApiResponse } from "@/types/api";
+import { auth } from "@clerk/nextjs/server";
+import { eq } from "drizzle-orm";
+import { NextRequest, NextResponse } from "next/server";
+
+export async function GET(req: NextRequest) {
+    try {
+        //1. Authentication check
+        const { userId } = await auth();
+        if(!userId) {
+            return NextResponse.json<ApiResponse<null>>({
+                success: false,
+                message: "Unauthorized",
+                data: null,
+                error: { code: "UNAUTHORIZED" }
+            }, { status: 401 });
+        }
+
+        //2. Authorization check (Database Role check)
+        // Best Practice: Always check DB for role, don't trust client-side claims
+        const user = await db.query.users.findFirst({
+            where: eq(users.id, userId),
+            columns: { role: true }
+        });
+
+        if(!user || user.role !== "admin") {
+            return NextResponse.json<ApiResponse<null>>({
+                success: false,
+                message: "Forbidden: Admin access required",
+                data: null,
+                error: { code: "FORBIDDEN" }
+            }, { status: 403 });
+        }
+
+        // Call Heavy Service 1
+        const response = await getRecentOrdersService();
+        let status = 200;
+        if(!response.success) {
+            status = response.error?.code === "INTERNAL_ERROR" ? 500 : 400;
+        }
+        return NextResponse.json<ApiResponse<RecentOrder[]>>(response, { status });
+    } catch (error) {
+       return NextResponse.json<ApiResponse<null>>({
+            success: false,
+            message: "Internal Server Error",
+            data: null,
+            error: { code: "INTERNAL_ERROR" }
+        }, { status: 500 });
+    }
+}
